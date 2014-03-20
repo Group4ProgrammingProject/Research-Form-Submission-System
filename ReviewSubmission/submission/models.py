4c43693b0fd48c424df1482cc390d77f80d3fbac
Ciaran Egan <eganc6@tcd.ie>
2014-03-20 14:00:38 +0000

    Added a comments app

--- ReviewSubmission/submission/models.py
+++ ReviewSubmission/submission/models.py
@@ -4,11 +4,11 @@ from time import time
 def get_upload_file_name(isntance, filename):
 	return "uploaded_files/%s_%s" % (str(time()).replace('.','_'),filename)
 
-# Create your models here.	
-class Submission(models.Model):
-	pub_date = models.DateTimeField('date published')
-	complete = models.BooleanField(default=False)
-	application = models.FileField(upload_to=get_upload_file_name)
+# Create your models here.
+# class Submission(models.Model):
+# 	pub_date = models.DateTimeField('date published')
+# 	complete = models.BooleanField(default=False)
+# 	application = models.FileField(upload_to=get_upload_file_name)
 
 
 #class PersonalSubmission(models.Model):
@@ -20,3 +20,34 @@ class Submission(models.Model):
 #class SubmissionVersion(models.Model):
 #	pub_date = models.DateTimeField('date published')
 #	most_recent = models.BooleanField(default=True) # needs to update to false if a newer version is submitted
+
+
+
+#Submission models to be copied into a new app. Couldnt run Django properly on a different pc so i'll swap it over later
+
+
+class Submission(models.Model):
+
+	AWAITING_COMPLETION_CHECK = 0
+	INCOMPLETE = 1
+	AWAITING_APPROVAL = 2
+	REJECTED = 3
+	APPROVED = 4
+
+	STATUS_CHOICE = (
+		(AWAITING_COMPLETION_CHECK, 'Being checked for completion'),
+		(INCOMPLETE, 'Incomplete'),
+		(AWAITING_APPROVAL, 'Awaiting Approval'),
+		(REJECTED, 'Rejected'),
+		(APPROVED, 'Approved'),
+	)
+
+	user = models.OneToOneField(User, unique=True)
+	status = models.IntegerField(default=AWAITING_COMPLETION_CHECK, choices=STATUS_CHOICE)
+
+
+class Version(models.Model):
+	application_form = models.FileField(upload_to=os.path.join(settings.PROJECT_DIR,''))
+	applicant_info = models.FileField(upload_to=os.path.join(settings.PROJECT_DIR, ''))
+	consent_form = models.FileField(upload_to=os.path.join(settings.PROJECT_DIR, ''))
+	submission = models.ForeignKey(Submission)
