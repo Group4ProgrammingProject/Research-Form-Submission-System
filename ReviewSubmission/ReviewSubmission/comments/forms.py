bab9c385e9eb98834822e43412f9f181ed01e3d9
leesg1 <leesg@tcd.ie>
2014-04-03 12:17:51 +0100

    Somehow I submitted the wrong thing again. This has the working comments section

--- ReviewSubmission/ReviewSubmission/comments/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from models import Message
-
-class CommentForm(forms.ModelForm):
-
-	class Meta:
-		model = Message
-		fields = ('user','message')
--- ReviewSubmission/submission/models.py
+++ ReviewSubmission/submission/models.py
@@ -8,24 +8,6 @@ from django.conf import settings
 def get_upload_file_name(isntance, filename):
 	return "uploaded_files/%s_%s" % (str(time()).replace('.','_'),filename)
 
-# Create your models here.
-# class Submission(models.Model):
-# 	pub_date = models.DateTimeField('date published')
-# 	complete = models.BooleanField(default=False)
-# 	application = models.FileField(upload_to=get_upload_file_name)
-
-
-#class PersonalSubmission(models.Model):
-
-#	pub_date = models.DateTimeField('date published')
-#	complete = models.BooleanField()
-#	application = models.FileField(upload_to=get_upload_file_name)
-
-#class SubmissionVersion(models.Model):
-#	pub_date = models.DateTimeField('date published')
-#	most_recent = models.BooleanField(default=True) # needs to update to false if a newer version is submitted
-
-
 class Submission(models.Model):
 
 	AWAITING_UPLOAD = 0
